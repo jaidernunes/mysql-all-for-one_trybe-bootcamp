a5dbf30a86684aa35b677fdd175b6e8f4be643b9
jaidernunes <jaidernf@gmail.com>
2023-02-04 17:07:06 -0300

    Req 1: updated

--- desafio1.sql
+++ desafio1.sql
@@ -28,34 +28,42 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
   CREATE TABLE SpotifyClone.follows(
       user_id INT NOT NULL,
-      follow_id INT NOT NULL,
+      follow_id INT,
       FOREIGN KEY (user_id) REFERENCES users(user_id,),
       FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
       CONSTRAINT PRIMARY KEY(user_id, artist_id)
   ) engine = InnoDB;
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
+
+  CREATE TABLE SpotifyClone.artists(
+      artist_id INT PRIMARY KEY AUTO_INCREMENT,
+      artist_name VARCHAR(150) NOT NULL,
   ) engine = InnoDB;
 
-  INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
+  CREATE TABLE SpotifyClone.albums(
+      album_id INT PRIMARY KEY AUTO_INCREMENT,
+      album_name VARCHAR(150) NOT NULL,
+      artist_id INT NOT NULL,
+      year_launch INT NOT NULL,
+      FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
+  ) engine = InnoDB;
 
-  INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+  CREATE TABLE SpotifyClone.songs(
+      song_id INT PRIMARY KEY AUTO_INCREMENT,
+      song_name VARCHAR(150) NOT NULL,
+      duration INT NOT NULL,
+      album_id INT NOT NULL,
+      artist_id INT NOT NULL,
+      FOREIGN KEY (album_id) REFERENCES albums(album_id)
+
+  ) engine = InnoDB;
+
+  -- INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
+  -- VALUES
+  --   ('exemplo de dados 1', 'exemplo de dados A'),
+  --   ('exemplo de dados 2', 'exemplo de dados B'),
+  --   ('exemplo de dados 3', 'exemplo de dados C');
+
+  -- INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
+  -- VALUES
+  --   ('exemplo de dados 1', 'exemplo de dados X'),
+  --   ('exemplo de dados 2', 'exemplo de dados Y');
